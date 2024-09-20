06af76567fb01bee9e004ed9f25e4a83ac844327
cubeiplkumar <emailanilkumarm@gmail.com>
2024-09-20 18:42:18 +0530

    feature202 changes

--- Application.java
+++ Application.java
@@ -18,4 +18,23 @@ class Main {
     int result = obj.addNumbers(num1, num2);
     System.out.println("Sum is: " + result);
   }
+  #feature202  by ethan in oct2023
+  class MethodOverloading {
+
+    // this method accepts int
+    private static void display(int a){
+        System.out.println("Got Integer data.");
+    }
+
+    // this method  accepts String object
+    private static void display(String a){
+        System.out.println("Got String object.");
+    }
+
+    public static void main(String[] args) {
+        display(1);
+        display("Hello");
+    }
+}
+#end of feature202
 }
